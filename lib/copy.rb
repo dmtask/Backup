77768410d4cb6fe94a76033cf61e2b6c36751354
Daniel Mertgen <dm@eoceo.de>
2017-04-03 17:18:01 +0200

    richtigen config name

--- lib/copy.rb
+++ lib/copy.rb
@@ -2,7 +2,7 @@ class Copy
   class << self
       public def copy
         configs = get_configs
-        full_path = configs['tmp_path'].to_s + configs['daniel-home'].to_s + '_*.tar.gz'
+        full_path = configs['tmp_path'].to_s + configs['backup_name'].to_s + '_*.tar.gz'
 
         FileUtils.cp(full_path, configs['backup_path'].to_s)
       end
