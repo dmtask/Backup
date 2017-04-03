62c1d8b3d4d7847af470f3de92135ab85fca9b1e
Daniel Mertgen <dm@eoceo.de>
2017-04-03 16:16:14 +0200

    verschlüsselung begonnen

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 Backupscript
+
+**Abhängigkeiten**:
+- https://github.com/phortx/ferrets-on-fire
+- https://github.com/rcook/rgpg
--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -1,5 +1,23 @@
 class Encrypt
   class << self
+    public def encrypt
 
+    end
+
+
+    private def generateKeyPair
+
+    end
+
+
+    private def get_configs
+      parsed = begin
+        YAML.load(File.open("config/backup.yml"))
+      rescue ArgumentError => e
+        error "Kann YAML Config Datei nicht lesen: #{e.message}"
+      end
+
+      parsed
+    end
   end
 end
