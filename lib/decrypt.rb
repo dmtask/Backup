6e095ed2644755335e819cb1b89e475486b31139
Daniel Mertgen <dm@eoceo.de>
2017-04-05 13:13:41 +0200

    decrypt prototypisch eingebaut

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -1,5 +1,9 @@
+require 'rgpg'
+
 class Decrypt
   class << self
-
+    public def decrypt(key_pub, key_sec, encrypted_file, file)
+      Rgpg::GpgHelper.decrypt_file key_pub, key_sec, encrypted_file, file, ['secret_key_passphrase']
+    end
   end
 end
