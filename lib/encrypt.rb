cbd0d0285468b341b88bc0e0c32d1cde256c5d2e
Daniel Mertgen <daniel.mertgen@gmx.de>
2017-06-26 16:54:22 +0200

    Verschlüsselung eingebaut

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -1,9 +1,14 @@
 class Encrypt
   class << self
+    # Archiv verschlüsseln
     public def encrypt(configs, backup_name)
       full_path = "#{configs['tmp_path']}#{backup_name}.tar.gz"
 
-      # TODO: Vom alten Script übernehmen...
+      # Ins Temp Verzeichnis wechseln
+      Dir.chdir(configs['tmp_path'].to_s)
+
+      # Verschlüsseln...
+      `gpg -z 0 -c "#{full_path}"`
     end
   end
 end
